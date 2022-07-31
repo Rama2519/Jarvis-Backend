535531f26dfc7f9cf3362aad106c71e3808af5c7
Rama Sathya Sai <ramasathyasai2519@gmail.com>
2022-07-31 10:35:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, jsonify, request
+import requests
 
 app = Flask(__name__)
 app.debug = True
 
+
 @app.route('/', methods=['GET', 'POST'])
-async def main():
-	if request.method == 'POST':
-		command = await request.form["sample"]
-		command = {'command': command}
-		print(command)
-		return jsonify(command)
-     
+def main():
+    if request.method == 'POST':
+        command = request.form["sample"]
+        command = {'command': command}
+        print(command)
+        requests.post('https://d6fb-49-37-156-187.in.ngrok.io/', command)
+        return ''
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
