f96ba1ed093f8eb560f2999501ee6d5c8b1c457f
Rama Sathya Sai <ramasathyasai2519@gmail.com>
2022-08-07 15:31:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ app.debug = True
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if request.method == 'POST':
-        command = request.form["sample"]
+        request_data = request.data  # getting the response data
+        request_data = json.loads(request_data.decode('utf-8'))  # converting it from json to key value pair
+        command = [request_data['sample']]
         command = {'command': command}
         print(command)
         requests.post('https://f7df-49-37-159-104.in.ngrok.io/', command)
